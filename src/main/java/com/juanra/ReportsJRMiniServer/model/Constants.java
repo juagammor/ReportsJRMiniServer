cff03ae8eb82090fb2e5d08fcf199707c8e0b5fb
juagammor <juanragamez@gmail.com>
2017-05-25 12:25:54 +0200

    Eliminadas todas las referencias a valores y pasadas a una clase de constantes.Fix #3

---
+++ src/main/java/com/juanra/ReportsJRMiniServer/model/Constants.java
@@ -0,0 +1,32 @@
+package com.juanra.ReportsJRMiniServer.model;
+
+/**
+ * Clase donde se aglutinan las constantes necesarias
+ * @author juanra
+ *
+ */
+public class Constants {
+
+	//Parametros por linea de comandos
+	public static final String PARAM_BBDD_DRIVER = "bbdd.driver";
+	public static final String PARAM_BBDD_URL = "bbdd.url";
+	public static final String PARAM_BBDD_USER = "bbdd.user";
+	public static final String PARAM_BBDD_PASS = "bbdd.pass";
+	public static final String PARAM_RUTA_JRXML = "rutaJRXML"; 
+	public static final String PARAM_RUTA_PDF = "rutaPDF" ;
+	public static final String PARAM_LISTA_PARAMS = "listaParametros"; 
+	public static final String PARAM_LISTA_IMAGENES = "listaImagenes";
+	//
+	public static final String ERROR_EXT = ".error";
+	//Formatos Fecha
+	public static final String DATE_FORMAT_DDMMYYYY = "dd/MM/yyyy";
+	//Caracteres
+	public static final String CHAR_PUNTO = ".";
+	public static final String CHAR_ARROBA = "@";
+	public static final String CHAR_ALMHOADILLA = "#";
+	public static final String CHAR_IGUAL = "=";
+	//
+	
+}
+
+
